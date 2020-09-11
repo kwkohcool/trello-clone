376e25fa279ebe44d2ababa9514f6e92d57f1290
kwkohcool <kwkoh_cool@hotmail.com>
2020-09-11 15:10:13 +0900

    add some dispatch logic

--- src/Column.tsx
+++ src/Column.tsx
@@ -7,23 +7,25 @@ import { useAppState } from "./AppStateContext";
 interface ColumnProps {
   text: string
   index: number
+  id: string
 }
 
 export const Column = ({
   text,
   index,
+  id
 }: ColumnProps) => {
-  const { state } = useAppState();
+  const { state, dispatch } = useAppState();
 
   return (
     <ColumnContainer>
       <ColumnTitle>{text}</ColumnTitle>
-      {state.lists[index].tasks.map(task => (
-        <Card text={task.text} key={task.id} />
+      {state.lists[index].tasks.map((task, i) => (
+        <Card text={task.text} key={task.id} index={i} />
       ))}
       <AddNewItem
-        toggleButtonText="+Add another task"
-        onAdd={console.log}
+        toggleButtonText="+ Add another task"
+        onAdd={text => dispatch({ type: "ADD_TASK", payload: { text, listId: id }})}
         dark
       />
     </ColumnContainer>
